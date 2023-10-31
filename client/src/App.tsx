64e9477844be4d7ab95840fd003e2a41b3a9ca0d
최정윤 <ej921004@naver.com>
2023-10-31 21:10:44 +0900

    Build : modify depoly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import './App.css';
-import {MainPage} from './pages';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { MainPage } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/iKU" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
