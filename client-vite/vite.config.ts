d3faa002354169c1cfa709f7c71ea5a722d70a26
최정윤 <ej921004@naver.com>
2023-11-01 16:50:18 +0900

    Build : rename client

--- client-vite/vite.config.ts
+++ client-vite/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
 import legacy from "@vitejs/plugin-legacy";
 import babel from "vite-plugin-babel";
+import commonjs from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
