088c95a4d960e37e5b37414d27aebe4e908864f8
최정윤 <ej921004@naver.com>
2023-11-04 07:02:47 +0900

    Feat : add navigate buttons at main

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { CheckList, DailyMission, DateInfo } from "../../components";
 import WheatherInfo from "../../components/WeatherInfo/WeatherInfo";
 import { Container, Line, RowContainer, Title } from "./styles";
 
 const MainPage = () => {
   const userName = "미러쿵야";
-
+  const navigate = useNavigate();
   return (
     <Container>
+      <div>-- 미션페이지 바로가기 --</div>
+      <div>음성인식 적용되면 버튼으로 된 것들은 모두 지울 예정</div>
+      <button onClick={() => navigate("/mission/clap")}>박수 치기 미션</button>
+      <button onClick={() => navigate("/mission/smile")}>웃기 미션</button>
+      <button onClick={() => navigate("/mission/side")}>
+        옆구리 스트레칭 미션
+      </button>
       <Title>{userName}님, 좋은 하루 입니다!</Title>
       <RowContainer>
         <CheckList />
