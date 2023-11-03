e81b49c0a8fa0e7e50f449a15db1c7f04ab37db5
최정윤 <ej921004@naver.com>
2023-11-04 06:44:13 +0900

    Feat : load DailyMissionItem

--- client/src/components/DailyMission/DailyMission.tsx
+++ client/src/components/DailyMission/DailyMission.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import { Container, Title } from "./styles";
+import { DailyMissionItem } from "../DailyMissionItem";
 const DailyMission = () => {
-  return <div>일일 미션</div>;
+  const [dailyMission, setDailyMission] = useState([
+    {
+      title: "박수 치기",
+      fulfilled: false,
+    },
+    {
+      title: "활짝 웃기",
+      fulfilled: false,
+    },
+    {
+      title: "옆구리 운동",
+      fulfilled: false,
+    },
+    {
+      title: "끝말잇기",
+      fulfilled: false,
+    },
+  ]);
+  return (
+    <Container>
+      <Title>일일미션</Title>
+      {dailyMission.map(({ title, fulfilled }, idx) => {
+        return (
+          <DailyMissionItem title={title} fulfilled={fulfilled} key={idx} />
+        );
+      })}
+    </Container>
+  );
 };
-
 export default DailyMission;
---
+++ client/src/components/DailyMission/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: bold;
+`;
+
+export { Container, Title };
