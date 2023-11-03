48fa00277ad8a0e698239ff100834f666ea1e161
최정윤 <ej921004@naver.com>
2023-11-04 06:44:33 +0900

    Feat : load DateInfo

--- client/src/components/DateInfo/DateInfo.tsx
+++ client/src/components/DateInfo/DateInfo.tsx
@@ -1,6 +1,27 @@
+import { Container, TimeText, DateText } from "./styles";
+import moment from "moment";
+
 const DateInfo = () => {
-  const date = new Date();
-  return <div>{date.toISOString()}</div>;
+  const today = moment();
+
+  // 요일 구하기
+  const returnDay = (day: number) => {
+    const days = ["일", "월", "화", "수", "목", "금", "토"];
+    return days[day];
+  };
+
+  const dayOrNignt = (a: string) => (a === "AM" ? "오전" : "오후");
+  return (
+    <Container>
+      <DateText>
+        {today.format(`YYYY년 MM월 DD일 ${returnDay(today.day())}요일`)}
+      </DateText>
+      <TimeText>
+        {dayOrNignt(today.format("A"))}
+        {today.format(" hh시 mm분")}
+      </TimeText>
+    </Container>
+  );
 };
 
 export default DateInfo;
---
+++ client/src/components/DateInfo/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const DateText = styled.div`
+  font-size: 2rem;
+`;
+
+export const TimeText = styled.div`
+  font-size: 1.5rem;
+`;
