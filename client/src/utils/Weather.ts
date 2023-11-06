11eacb2817587ca768f4a34dec8b003eeca96159
최정윤 <ej921004@naver.com>
2023-11-06 20:19:16 +0900

    Feat : fix position

--- client/src/utils/Weather.ts
+++ client/src/utils/Weather.ts
@@ -8,20 +8,24 @@ interface Position {
 }
 
 const getCurrentLocationWeather = async () => {
+  let latitude = 37.543561771936005;
+  let longitude = 127.07741808265483;
   try {
     const position: Position = await new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject);
     });
-
-    const { latitude, longitude } = position.coords;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
     console.log(`lat : ${latitude}, long : ${longitude}`);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&lang=kr`;
 
     const response = await axios.get(url);
+
     return response.data;
-  } catch (err) {
-    console.error(err);
-    throw err;
   }
 };
 
