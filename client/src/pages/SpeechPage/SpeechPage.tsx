a226871c7d6ee3be90c0bc48ef79a59e908756b5
최정윤 <ej921004@naver.com>
2023-11-07 14:29:00 +0900

    Feat: speech test

--- client/src/pages/SpeechPage/SpeechPage.tsx
+++ client/src/pages/SpeechPage/SpeechPage.tsx
@@ -1,46 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const SpeechPage = () => {
-  useEffect(() => {
-    const SpeechRecognition =
-      window.SpeechRecognition || window.webkitSpeechRecognition;
+  const TextToSpeech = () => {
+    const [text, setText] = useState("");
 
-    if (SpeechRecognition) {
-      const recognition = new SpeechRecognition();
-      console.log(recognition);
-      recognition.lang = "ko-KR"; // 한국어로 설정
-      recognition.continuous = true; // 연속적인 음성 인식 활성화
+    const speak = () => {
+      if (text !== "") {
+        const speechSynthesis = window.speechSynthesis;
+        const utterance = new SpeechSynthesisUtterance(text);
+        speechSynthesis.speak(utterance);
+      }
+    };
 
-      recognition.onresult = (event) => {
-        for (let i = event.resultIndex; i < event.results.length; ++i) {
-          if (event.results[i].isFinal) {
-            const text = event.results[i][0].transcript;
-            console.log("당신이 말한 것: ", text);
-          }
-        }
-      };
+    return (
+      <div>
+        <textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter text to read..."
+        />
+        <button onClick={speak}>Speak</button>
+      </div>
+    );
+  };
 
-      recognition.onerror = (event) => {
-        console.error("음성 인식 오류", event);
-      };
-
-      recognition.start();
-
-      // 컴포넌트가 언마운트될 때 음성 인식을 중지
-      return () => {
-        recognition.stop();
-      };
-    } else {
-      console.error(
-        "이 브라우저에서는 SpeechRecognition API를 사용할 수 없습니다."
-      );
-    }
-  }, []);
-  return (
-    <div>
-      <p>왜왜</p>
-    </div>
-  );
+  return <TextToSpeech />;
 };
 
 export default SpeechPage;
