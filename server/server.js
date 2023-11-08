6640fd0f2dc8f36e7fbde91c01e56caba93ab8e2
최정윤 <ej921004@naver.com>
2023-11-08 22:46:45 +0900

    Feat : add middleware using speech recognition

--- server/server.js
+++ server/server.js
@@ -29,13 +29,64 @@ app.get("/detect/:mission", (req, res) => {
   pythonProcess.stdout.on("data", (data) => {
     const message = data.toString();
 
-    if (message.includes("Camera started")) {
+    if (message.includes("loading")) {
+      res.write(`data: ${JSON.stringify({ event: "loading" })}\n\n`);
+    } else if (message.includes("Camera started")) {
       // 카메라 시작 메시지를 처리합니다.
       console.log("Camera has started.");
       res.write(`data: ${JSON.stringify({ event: "camera-started" })}\n\n`);
     } else {
       res.write(
         `data: ${JSON.stringify({ event: "result", data: message })}\n\n`
+      );
+    }
+  });
+
+  pythonProcess.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  pythonProcess.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+    res.write(`data: ${JSON.stringify({ event: "close", code: code })}\n\n`); // 종료 메시지
+    res.end();
+  });
+});
+
+app.get("/speechRecognition", (req, res) => {
+  res.writeHead(200, eventStreamHeaderOption);
+
+  const pythonProcess = spawn("python3", [`src/speechRecognition.py`]);
+
+  pythonProcess.stdout.on("data", (data) => {
+    const message = data.toString();
+
+    if (message.includes("Recognition Start")) {
+      // 음성인식 시작 후 키워드 "거울아"를 기다립니다.
+      res.write(
+        `data: ${JSON.stringify({ event: "recognition-started" })}\n\n`
+      );
+    } else if (message.includes("Routing Start")) {
+      res.write(
+        `data: ${JSON.stringify({ event: "routing-start", data: message })}\n\n`
+      );
+    } else if (message.includes("is defined function")) {
+      const resultMessage = message.split(">");
+
+      res.write(
+        `data: ${JSON.stringify({
+          event: "routing",
+          data: resultMessage[0],
+        })}\n\n`
+      );
+    } else if (message.includes("is not defined function")) {
+      const resultMessage = message.split(">");
+      console.log(resultMessage);
+      res.write(
+        `data: ${JSON.stringify({
+          event: "routing-failed",
+          data: resultMessage[0],
+        })}\n\n`
       );
     }
   });
