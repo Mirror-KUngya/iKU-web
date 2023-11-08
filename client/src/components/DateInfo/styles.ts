53291e91e9af8674d7adb46068deb5a4e22d7563
최정윤 <ej921004@naver.com>
2023-11-09 02:20:30 +0900

    Design : add styles at DateInfo

--- client/src/components/DateInfo/styles.ts
+++ client/src/components/DateInfo/styles.ts
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 60%;
+  height: 180px;
+  justify-content: space-between;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
 
 export const DateText = styled.div`
-  font-size: 2rem;
+  font-size: 3.2rem;
 `;
 
 export const TimeText = styled.div`
-  font-size: 1.5rem;
+  font-size: 5rem;
 `;
