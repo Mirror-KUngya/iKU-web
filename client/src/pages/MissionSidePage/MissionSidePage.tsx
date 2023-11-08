5f9b49942ce72a83373882cb1f01ff2f1479a7c9
최정윤 <ej921004@naver.com>
2023-11-08 22:37:53 +0900

    Feat : add voice at MissionSidePage

--- client/src/pages/MissionSidePage/MissionSidePage.tsx
+++ client/src/pages/MissionSidePage/MissionSidePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MissionStatus } from "../../utils";
 import {
   Button,
@@ -9,7 +9,6 @@ import {
 import { MissionInfo } from "../../components";
 
 const MissionSidePage = () => {
-  const ENDPOINT = "http://localhost:5001";
   const [status, setStatus] = useState(MissionStatus.DEFAULT);
   const [leftCount, setLeftCount] = useState(0);
   const [rightCount, setRightCount] = useState(0);
@@ -18,18 +17,22 @@ const MissionSidePage = () => {
   const runSidePythonScript = () => {
     setLeftCount(0);
     setRightCount(0);
-    setStatus(MissionStatus.READY);
 
-    const eventSource = new EventSource(ENDPOINT + "/detect/side");
+    const eventSource = new EventSource(
+      process.env.REACT_APP_API_ENDPOINT + "/detect/side"
+    );
     eventSource.onmessage = (event) => {
       const data = JSON.parse(event.data);
 
-      if (data.event === "camera-started") {
+      if (data.event === "loading") {
+        setStatus(MissionStatus.READY);
+      } else if (data.event === "camera-started") {
         setStatus(MissionStatus.RUNNING);
       } else if (data.event === "result") {
-        setMissionResult(true);
         if (data.data.trim() === "left") setLeftCount((cur) => cur + 1);
         else if (data.data.trim() === "right") setRightCount((cur) => cur + 1);
+
+        if (leftCount >= 2 && rightCount >= 2) setMissionResult(true);
       } else if (data.event === "close") {
         eventSource.close();
         setStatus(MissionStatus.END);
@@ -42,32 +45,40 @@ const MissionSidePage = () => {
     };
   };
 
+  console.log(status);
+  useEffect(() => {
+    runSidePythonScript();
+  }, []);
+
   return (
     <Container>
       <MissionInfo
         title="옆구리 운동"
-        description={`거울을 정면으로 보고\n다음 동작을 따라해주세요!`}
-        images={[
-          `${process.env.PUBLIC_URL}/image/side1.png`,
-          `${process.env.PUBLIC_URL}/image/side2.png`,
-          `${process.env.PUBLIC_URL}/image/side3.png`,
-        ]}
+        description={`그림과 같이 거울을 정면으로 보고\n왼쪽 2번, 오른쪽 2번씩 각각 옆구리 운동을 해주세요!`}
+        // images={[
+        //   `${process.env.PUBLIC_URL}/image/side1.png`,
+        //   `${process.env.PUBLIC_URL}/image/side2.png`,
+        //   `${process.env.PUBLIC_URL}/image/side3.png`,
+        // ]}
+        images={[`${process.env.PUBLIC_URL}/image/side.gif`]}
       />
       <MissionStatusText>
-        {status === MissionStatus.READY
+        {status === MissionStatus.DEFAULT
+          ? " "
+          : status === MissionStatus.READY
           ? " 카메라를 불러오는 중입니다. 잠시만 기다려 주세요 . . ."
           : status === MissionStatus.RUNNING
           ? " 미션을 진행해주세요 . . ."
           : status === MissionStatus.END
           ? ` 미션 ${missionResult ? "성공!" : "실패"}`
-          : "시작 버튼을 눌러 미션을 시작하세요! "}
+          : "에러 발생, 네트워크를 확인해주세요. "}
       </MissionStatusText>
       <CurrentStatusText>왼쪽 횟수 : {leftCount}</CurrentStatusText>
       <CurrentStatusText>오른쪽 횟수 : {rightCount}</CurrentStatusText>
 
-      <Button onClick={runSidePythonScript}>
+      {/* <Button onClick={runSidePythonScript}>
         <span>미션 시작</span>
-      </Button>
+      </Button> */}
     </Container>
   );
 };
