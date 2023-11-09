eec43501bc02feba6275c0639994a6fc97788dd2
최정윤 <ej921004@naver.com>
2023-11-09 16:48:24 +0900

    Feat : modify smile py file

--- server/src/smile.py
+++ server/src/smile.py
@@ -3,6 +3,7 @@ import cv2
 import mediapipe as mp
 import sys
 import collections
+from picamera2 import Picamera2
 from playsound import playsound
 
 playsound("voice/smile_start.mp3")
@@ -16,13 +17,17 @@ face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1)
 drawing_spec = mp.solutions.drawing_utils.DrawingSpec(thickness=1, circle_radius=1)
 
 # 웹캠에서 비디오를 가져오기
-cap = cv2.VideoCapture(1)
+# cap = cv2.VideoCapture(1)
 
-# 프레임레이트를 확인
-fps = cap.get(cv2.CAP_PROP_FPS)
-if fps < 1:
-    # 대체 프레임레이트 설정
-    fps = 30
+# Picamera2 시작
+picam2 = Picamera2()
+picam2.start_preview()
+
+fps = 30
+# 카메라 설정
+camera_config = picam2.create_preview_configuration(main={"size": (1280, 720)})
+picam2.configure(camera_config)
+picam2.start()
 
 # 이전 입 너비와 입꼬리 상대 y좌표를 저장할 버퍼 (1초분량)
 prev_mouth_widths = collections.deque(maxlen=int(fps))
@@ -31,30 +36,45 @@ prev_mouth_corners_relative_y = collections.deque(maxlen=int(fps) * 2)  # 양쪽
 # 웃음 감지를 위한 임계값 설정
 mouth_width_increase_threshold = 0.02  # 입 너비 증가 임계값
 mouth_corner_lift_threshold = 0.008  # 입꼬리 상대적 상승 임계값
+return_to_neutral_threshold = -0.015  # 입꼬리 하락 임계값
+
+# 웃는 표정 지속 시간을 측정하기 위한 변수 초기화
+smile_start_time = None  # 웃기 시작한 시간
+smiling_time = 0  # 웃은 총 시간
+start_time = time.time()  # 프로그램 시작 시간
 
 
-camera_started_flag = False  # 카메라 시작 플래그 초기화
+while True:
+    # ret, frame = cap.read()
+    # if not ret:
+    #     continue
 
-start_time = time.time()  # 시작 시간 기록
+    # if not camera_started_flag:  # 카메라 시작 플래그를 체크합니다.
+    #     print("Camera started")  # 카메라가 시작되면 이 메시지를 출력합니다.
+    #     sys.stdout.flush()
+    #     camera_started_flag = True  # 플래그를 설정하여 메시지가 다시 출력되지 않도록 합니다.
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        continue
+    # if time.time() - start_time > 5:  # 5초 후 종료 조건 검사
+    #     break
 
-    if not camera_started_flag:  # 카메라 시작 플래그를 체크합니다.
-        print("Camera started")  # 카메라가 시작되면 이 메시지를 출력합니다.
-        sys.stdout.flush()
-        camera_started_flag = True  # 플래그를 설정하여 메시지가 다시 출력되지 않도록 합니다.
+    # # RGB로 변환
+    # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    if time.time() - start_time > 5:  # 5초 후 종료 조건 검사
+    # # 얼굴 랜드마크 감지
+    # results = face_mesh.process(rgb_frame)
+
+    # Picamera2로부터 이미지 프레임 받아오기
+    frame = picam2.capture_array()
+    if frame is None:
+        print("Error receiving frame from Picamera2.")
         break
 
-    # RGB로 변환
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(image_rgb)
 
-    # 얼굴 랜드마크 감지
-    results = face_mesh.process(rgb_frame)
+    image_bgr = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
+    # 웃음 감지
+    is_smiling = False
 
     # # 감지된 랜드마크가 있는 경우
     if results.multi_face_landmarks:
@@ -73,6 +93,11 @@ while cap.isOpened():
             current_mouth_width = abs(right_mouth_corner.x - left_mouth_corner.x)
             current_left_corner_relative_y = left_mouth_corner.y - mouth_center_y
             current_right_corner_relative_y = right_mouth_corner.y - mouth_center_y
+
+            # 이전 데이터 저장
+            prev_mouth_widths.append(current_mouth_width)
+            prev_mouth_corners_relative_y.append(current_left_corner_relative_y)
+            prev_mouth_corners_relative_y.append(current_right_corner_relative_y)
 
             # 1초 전의 프레임과 입꼬리 상대 위치 및 너비 비교
             if len(prev_mouth_widths) >= 1 and len(prev_mouth_corners_relative_y) >= 2:
@@ -107,22 +132,79 @@ while cap.isOpened():
                     )
                     print("smile", flush=True)  # 표준 출력으로 "Smiling" 메시지 출력
 
+                elif (
+                    mouth_width_change < return_to_neutral_threshold
+                    and mouth_corners_lift < return_to_neutral_threshold
+                ):
+                    if smile_start_time is not None:
+                        # smiling_time += time.time() - smile_start_time  # 웃은 시간 누적
+                        smile_start_time = None  # 웃음 시간 기록 리셋
+
                 else:
                     print("not", flush=True)
 
-                    # 현재 프레임의 데이터를 저장
-            prev_mouth_widths.append(current_mouth_width)
-            prev_mouth_corners_relative_y.append(current_left_corner_relative_y)
-            prev_mouth_corners_relative_y.append(current_right_corner_relative_y)
-
-            # 얼굴 랜드마크 그리기 (기본 DrawingSpec 사용)
             mp.solutions.drawing_utils.draw_landmarks(
-                frame,
+                image_rgb,
                 landmarks,
                 mp_face_mesh.FACEMESH_CONTOURS,
-                drawing_spec,
-                drawing_spec,
+                landmark_drawing_spec=drawing_spec,
             )
+    # 웃음 지속 시간 표시
+    if smile_start_time is not None:
+        # 현재 웃고 있는 경우 지속 시간 업데이트
+        current_smiling_time = time.time() - smile_start_time
+        smiling_time = previous_smiling_time + current_smiling_time
+        cv2.putText(
+            image_rgb,
+            f"Smiling for: {current_smiling_time:.2f} sec",
+            (50, 150),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            2,
+        )
+        cv2.putText(
+            image_rgb,
+            f"Total smiling time: {smiling_time:.2f} sec",
+            (50, 100),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            2,
+        )
+
+    else:
+        # 웃음이 멈춘 경우 누적된 웃음 시간 표시
+        previous_smiling_time = smiling_time
+
+        cv2.putText(
+            image_rgb,
+            f"Total smiling time: {smiling_time:.2f} sec",
+            (50, 100),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            2,
+        )
+
+    # 화면에 결과 표시
+    cv2.imshow("Smile Detector", image_rgb)
+
+    # 프로그램 종료 조건 확인
+    current_time = time.time()
+    elapsed_time = current_time - start_time  # 프로그램 총 경과 시간 계산
+
+    if smiling_time >= 5:
+        print("mission success")
+        break
+
+    if elapsed_time >= 15:
+        print("mission failed")
+        break
+
+    # 'q'를 누르면 반복문 탈출
+    if cv2.waitKey(5) & 0xFF == ord("q"):
+        break
     # 화면에 결과 표시
     cv2.imshow("Face Landmarks", frame)
 
@@ -131,4 +213,10 @@ while cap.isOpened():
     #     break
 
 # 자원 해제
-cap.release()
+# cap.release()
+
+# 자원 해제
+picam2.stop_preview()
+picam2.stop()
+cv2.destroyAllWindows()
+face_mesh.close()
