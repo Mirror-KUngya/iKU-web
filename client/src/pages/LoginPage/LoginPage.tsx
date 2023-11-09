af10d6b5a886d536403f0eb18ca7034085851b5e
최정윤 <ej921004@naver.com>
2023-11-09 16:21:09 +0900

    Feat : add LoginPage UI

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,49 @@
-import { Container } from "../../commonStyles";
+import { useState } from "react";
+import {
+  Image,
+  Container,
+  BottomContainer,
+  Input,
+  LoginButton,
+  ButtonContainer,
+  LinkButton,
+} from "./styles";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  return <Container>로그인</Container>;
+  const navigate = useNavigate();
+  const [userId, setUserID] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    navigate("/");
+  };
+
+  return (
+    <Container>
+      <Image src={`${process.env.PUBLIC_URL}/logo.png`}></Image>
+      <BottomContainer>
+        <Input
+          placeholder={"아이디 (이메일)"}
+          value={userId}
+          type="email"
+          onChange={(e) => setUserID(e.target.value)}
+        ></Input>
+        <Input
+          placeholder={"비밀번호"}
+          type="password"
+          value={userId}
+          onChange={(e) => setUserID(e.target.value)}
+        ></Input>
+        <LoginButton onClick={handleLogin}>로그인</LoginButton>
+        <ButtonContainer>
+          <LinkButton>아이디찾기</LinkButton>
+          <LinkButton>|</LinkButton>
+          <LinkButton>비밀번호 변경</LinkButton>
+        </ButtonContainer>
+      </BottomContainer>
+    </Container>
+  );
 };
 
 export default LoginPage;
--- client/src/pages/LoginPage/styles.ts
+++ client/src/pages/LoginPage/styles.ts
@@ -1,1 +1,65 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  text-align: center;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20% 0;
+`;
+
+export const Image = styled.img`
+  width: 60%;
+  margin: 20px;
+`;
+
+export const BottomContainer = styled.div`
+  display: flex;
+  width: 80%;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px auto;
+`;
+
+export const Input = styled.input`
+  width: 90%;
+  padding: 30px;
+  margin: 10px 0;
+  font-size: 3rem;
+  font-weight: bold;
+  background-color: transparent;
+  border: none;
+  border-bottom: 3px solid white;
+  color: white;
+  &:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 500px black inset !important;
+  }
+`;
+
+export const LoginButton = styled.button`
+  width: 100%;
+  margin: 20px auto;
+  text-align: center;
+  font-size: 2.5rem;
+  font-weight: bold;
+  padding: 20px;
+  border-radius: 20px;
+  cursor: pointer;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  font-size: 1.8rem;
+`;
+
+export const LinkButton = styled.button`
+  background-color: transparent;
+  color: white;
+  font-size: 2.2rem;
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+`;
