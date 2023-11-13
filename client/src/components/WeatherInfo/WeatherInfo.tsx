a16fcce2b7ab88ec7c3a18b7cbc2f5ec772164de
최정윤 <ej921004@naver.com>
2023-11-13 16:17:41 +0900

    Feat : modify useHook

--- client/src/components/WeatherInfo/WeatherInfo.tsx
+++ client/src/components/WeatherInfo/WeatherInfo.tsx
@@ -28,23 +28,18 @@ const WheatherInfo = () => {
 
   useEffect(() => {
     console.log("temp : ", temp);
-    if (temp >= 28) usePatchWeatherCheckList("반팔, 반바지를 추천드려요.");
-    else if (temp >= 23)
-      usePatchWeatherCheckList("반팔, 얇은 셔츠, 면바지를 추천드려요.");
-    else if (temp >= 20)
-      usePatchWeatherCheckList("블라우스, 긴팔 티, 면바지를 추천드려요.");
-    else if (temp >= 17)
-      usePatchWeatherCheckList("얇은 가디건, 스웨터, 긴바지를 추천드려요.");
-    else if (temp >= 12)
-      usePatchWeatherCheckList("자켓, 가디건, 스웨터, 긴바지를 추천드려요.");
-    else if (temp >= 9)
-      usePatchWeatherCheckList("잠바, 코트 ,기모바지를 추천드려요.");
-    else if (temp >= 5)
-      usePatchWeatherCheckList("코트, 발열내의, 가죽 옷, 기모를 추천드려요.");
+    let content = "";
+    if (temp >= 28) content = "반팔, 반바지를 추천드려요.";
+    else if (temp >= 23) content = "반팔, 얇은 셔츠, 면바지를 추천드려요.";
+    else if (temp >= 20) content = "블라우스, 긴팔 티, 면바지를 추천드려요.";
+    else if (temp >= 17) content = "얇은 가디건, 스웨터, 긴바지를 추천드려요.";
+    else if (temp >= 12) content = "자켓, 가디건, 스웨터, 긴바지를 추천드려요.";
+    else if (temp >= 9) content = "잠바, 코트 ,기모바지를 추천드려요.";
+    else if (temp >= 5) content = "코트, 발열내의, 가죽 옷, 기모를 추천드려요.";
     else if (temp < 5)
-      usePatchWeatherCheckList(
-        "패딩, 두거운 코트, 누빔 옷, 기모, 목도리를 추천드려요."
-      );
+      content = "패딩, 두거운 코트, 누빔 옷, 기모, 목도리를 추천드려요.";
+
+    usePatchWeatherCheckList(content);
   }, [temp]);
 
   return (
