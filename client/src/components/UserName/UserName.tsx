b7df60ea98d54b8233e5f8cc8ec75f57f78b78ae
최정윤 <ej921004@naver.com>
2023-11-17 15:40:35 +0900

    Chore : modify username

--- client/src/components/UserName/UserName.tsx
+++ client/src/components/UserName/UserName.tsx
@@ -1,17 +1,22 @@
-import { useEffect } from "react";
-import { useGetUserName } from "../../hooks";
+// import { useEffect } from "react";
+// import { useGetUserName } from "../../hooks";
 import { HeaderText } from "./styles";
 
 const UserName = () => {
-  const { mutate, data } = useGetUserName();
+  // const { mutate, data } = useGetUserName();
 
-  useEffect(() => {
-    mutate();
+  // useEffect(() => {
+  //   mutate();
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-  return <HeaderText>{data}님, 좋은 하루 입니다!</HeaderText>;
+  return (
+    <HeaderText>
+      {/* {data}님, 좋은 하루 입니다! */}
+      승현 지은 채은 정윤 수고 많았어 ~~~
+    </HeaderText>
+  );
 };
 
 export default UserName;
